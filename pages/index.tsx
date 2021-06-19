febfac8bc546f51310f4f5229463cd1a545b9ca1
yuizho <yuizho3@gmail.com>
2021-06-19 14:59:21 +0900

    modify index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,15 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 
 const Home: FC = () => (
-    <>
-      <Head>
-        <title>blog</title>
-      </Head>
-      <h1>Hi!!!!!</h1>
-      <Link href="/contents/1">content1</Link>
-      <br />
-      <Link href="/contents/2">content2</Link>
-    </>
-  );
+  <>
+    <Head>
+      <title>blog</title>
+    </Head>
+    <h1>Hi!!!!!test</h1>
+    <Link href="/contents/1">content1</Link>
+    <br />
+    <Link href="/contents/2">content2</Link>
+  </>
+);
 
 export default Home;
