f46eab445fde5f5af4996e0807d543c13a9fd3df
yuizho <yuizho3@gmail.com>
2021-06-19 15:00:59 +0900

    modify index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: FC = () => (
     <Head>
       <title>blog</title>
     </Head>
-    <h1>Hi!!!!!test</h1>
+    <h1>Hi!!!!!testaaaaa</h1>
     <Link href="/contents/1">content1</Link>
     <br />
     <Link href="/contents/2">content2</Link>
