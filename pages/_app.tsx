e69c0198a8ef097c002b8b2b75da364d4bce5ea4
yuizho <yuizho3@gmail.com>
2021-06-19 17:34:40 +0900

    modify pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,7 @@ import { FC } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 export default MyApp;
