14556e249ce947968011a838922b4b404b31b9cd
yuizho <yuizho3@gmail.com>
2021-06-20 16:45:06 +0900

    modify Header component to apply css

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,11 +1,15 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 const Header: FC = () => (
-    <>
-      <nav>
-        <h1>Header Title</h1>
-      </nav>
-    </>
-  );
+  <>
+    <nav className="flex items-center justify-between flex-wrap bg-blue-900 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <span className="font-semibold text-xl tracking-tight">
+          J&apos;aime les ramens
+        </span>
+      </div>
+    </nav>
+  </>
+);
 
 export default Header;
