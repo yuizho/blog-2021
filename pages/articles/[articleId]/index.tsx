6dcdb6430b82f5c546779461906bd76ce5b75444
yuizho <yuizho3@gmail.com>
2021-06-21 23:56:19 +0900

    implement ArticleDetails page

--- pages/articles/[articleId]/index.tsx
+++ pages/articles/[articleId]/index.tsx
@@ -1,16 +1,86 @@
-import { useRouter } from 'next/dist/client/router';
+import { GetStaticProps } from 'next';
 import React, { FC } from 'react';
-import Header from '../../../components/organisms/Header';
+import { ParsedUrlQuery } from 'querystring';
+import Article from '../../../components/organisms/Article';
 
-const Content: FC = () => {
-  const router = useRouter();
-
-  return (
-    <>
-      <Header />
-      <div> {router.query.articleId}</div>
-    </>
-  );
+type Props = {
+  content: Content;
 };
 
-export default Content;
+interface Params extends ParsedUrlQuery {
+  articleId: string;
+}
+
+type Content = {
+  id: string;
+  title: string;
+  body: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
+
+const ArticleDetails: FC<Props> = ({ content }: Props) => <Article title={content.title} body={content.body} />;
+
+export const getStaticPaths = async () => {
+  console.log('getStaticPaths');
+  const requestHeaders = new Headers();
+  requestHeaders.set('X-API-KEY', process.env.API_KEY || '');
+  const key = {
+    headers: requestHeaders,
+  };
+
+  // TODO: Error handling
+  const result = await fetch(
+    'https://yuizho.microcms.io/api/v1/articles?fields=id',
+    key,
+  )
+    .then((res) => res.json())
+    .then(
+      (json) =>
+        json as {
+          contents: Array<{ id: string }>;
+          totalCount: number;
+          offset: number;
+          limit: number;
+        },
+    );
+
+  return {
+    fallback: false, // TODO: falseのままだとダメそう
+    paths: result.contents.map((content) => ({
+      params: {
+        articleId: content.id,
+      },
+    })),
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props, Params> = async (
+  context,
+) => {
+  const articleId = context.params?.articleId || '';
+
+  const requestHeaders = new Headers();
+  requestHeaders.set('X-API-KEY', process.env.API_KEY || '');
+  const key = {
+    headers: requestHeaders,
+  };
+
+  // TODO: Error handling
+  const result = await fetch(
+    `https://yuizho.microcms.io/api/v1/articles/${articleId}`,
+    key,
+  )
+    .then((res) => res.json())
+    .then((json) => json as Content);
+
+  return {
+    props: {
+      content: result,
+    },
+  };
+};
+
+export default ArticleDetails;
