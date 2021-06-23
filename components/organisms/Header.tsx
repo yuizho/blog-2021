19c824452d83e19cdd562b464d3fcb891260c2ac
yuizho <yuizho3@gmail.com>
2021-06-23 14:21:17 +0900

    modify Header component

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react';
+import Link from 'next/link';
 
 const Header: FC = () => (
   <>
-    <nav className="flex items-center justify-between flex-wrap bg-blue-900 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <span className="font-semibold text-xl tracking-tight">
-          J&apos;aime les ramens
-        </span>
+    <nav className="flex items-center justify-between flex-wrap  p-6">
+      <div className="flex items-center flex-shrink-0 text-gray-700 mr-6">
+        <Link href="/">
+          <a className="font-semibold text-xl tracking-tight hover:underline">
+            J&apos;aime les ramens
+          </a>
+        </Link>
       </div>
     </nav>
   </>
