cc99b1e434991db41a015c3c25ad11893060eddf
yuizho <yuizho3@gmail.com>
2021-06-23 14:20:22 +0900

    implement Article component

--- components/organisms/Article.tsx
+++ components/organisms/Article.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import styles from './Article.module.css';
 
 type Props = {
   title: string;
@@ -7,12 +8,19 @@ type Props = {
 
 const Article: FC<Props> = ({ title, body }: Props) => (
     <>
-      <h1 className="font-semibold text-l tracking-tight">{title}</h1>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${body}`,
-        }}
-      />
+      <div className={styles.container}>
+        <h1 className="text-gray-700 text-xl  font-semibold text-l tracking-tight">
+          {title}
+        </h1>
+        <div className="mt-2 text-sm  text-gray-600">2021-06-02</div>
+        <hr className="my-5" />
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{
+            __html: `${body}`,
+          }}
+        />
+      </div>
     </>
   );
 
