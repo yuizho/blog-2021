f775fb7e4785c0492ef4a6db5f5a2eb3b63dfde6
yuizho <yuizho3@gmail.com>
2021-09-24 22:45:01 +0900

    add GTM config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,24 @@ import { FC } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || '';
+
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <title>J'aime les ramens</title>
+      {/* Google Tag Manager*/}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
     </Head>
 
     <Component {...pageProps} />
